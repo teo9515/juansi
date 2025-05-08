5470813b8758cd6317178b59d7e82ef9c577fe7d
Mateo <teo9515@gmail.com>
2025-05-08 18:33:07 -0500

    fix

--- src/components/ProjectGalleryMobileTablet.tsx
+++ src/components/ProjectGalleryMobileTablet.tsx
@@ -6,7 +6,7 @@ import { projects } from "@/data/projects";
 
 const ProjectGalleryMobileTablet: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 w-11/12 justify-self-center gap-6 py-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 w-full justify-self-center gap-6 py-4">
       {projects.map((project) => (
         <div key={project.id} className="flex flex-col overflow-hidden">
           <Link
